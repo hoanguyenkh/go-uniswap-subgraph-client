bf66238b81a3f64b6fa3f67a1ea51e5ecab2d29f
Emerson Macro <emersonmacro@gmail.com>
2023-09-09 22:56:05 -0700

    feat: add factory model

--- models.go
+++ models.go
@@ -1,6 +1,42 @@
 package unigraphclient
 
-type PoolResult struct {
+type FactoryResponse struct {
+	Factory Factory
+}
+
+type Factory struct {
+	ID                           string
+	PoolCount                    string
+	TxCount                      string
+	TotalVolumeUSD               string
+	TotalVolumeETH               string
+	TotalFeesUSD                 string
+	TotalFeesETH                 string
+	UntrackedVolumeUSD           string
+	TotalValueLockedUSD          string
+	TotalValueLockedETH          string
+	TotalValueLockedUSDUntracked string
+	TotalValueLockedETHUntracked string
+	Owner                        string
+}
+
+var FactoryFields []string = []string{
+	"id",                           // ID!
+	"poolCount",                    // BigInt!
+	"txCount",                      // BigInt!
+	"totalVolumeUSD",               // BigDecimal!
+	"totalVolumeETH",               // BigDecimal!
+	"totalFeesUSD",                 // BigDecimal!
+	"totalFeesETH",                 // BigDecimal!
+	"untrackedVolumeUSD",           // BigDecimal!
+	"totalValueLockedUSD",          // BigDecimal!
+	"totalValueLockedETH",          // BigDecimal!
+	"totalValueLockedUSDUntracked", // BigDecimal!
+	"totalValueLockedETHUntracked", // BigDecimal!
+	"owner",                        // ID!
+}
+
+type PoolResponse struct {
 	Pool Pool
 }
 
@@ -38,42 +74,46 @@ type Pool struct {
 }
 
 var PoolFields []string = []string{
-	"id",
-	"createdAtTimestamp",
-	"createdAtBlockNumber",
-	// "token0",
-	// "token1",
-	"feeTier",
-	"liquidity",
-	"sqrtPrice",
-	"feeGrowthGlobal0X128",
-	"feeGrowthGlobal1X128",
-	"token0Price",
-	"token1Price",
-	"tick",
-	"observationIndex",
-	"volumeToken0",
-	"volumeToken1",
-	"volumeUSD",
-	"untrackedVolumeUSD",
-	"feesUSD",
-	"txCount",
-	"collectedFeesToken0",
-	"collectedFeesToken1",
-	"collectedFeesUSD",
-	"totalValueLockedToken0",
-	"totalValueLockedToken1",
-	"totalValueLockedETH",
-	"totalValueLockedUSD",
-	"totalValueLockedUSDUntracked",
-	"liquidityProviderCount",
-	// "poolHourData",
-	// "poolDayData",
-	// "mints",
-	// "burns",
-	// "swaps",
-	// "collects",
-	// "ticks",
+	"id",                   // ID!
+	"createdAtTimestamp",   // BigInt!
+	"createdAtBlockNumber", // BigInt!
+	// "token0", // Token!
+	// "token1", // Token!
+	"feeTier",                      // BigInt!
+	"liquidity",                    // BigInt!
+	"sqrtPrice",                    // BigInt!
+	"feeGrowthGlobal0X128",         // BigInt!
+	"feeGrowthGlobal1X128",         // BigInt!
+	"token0Price",                  // BigDecimal!
+	"token1Price",                  // BigDecimal!
+	"tick",                         // BigInt!
+	"observationIndex",             // BigInt!
+	"volumeToken0",                 // BigDecimal!
+	"volumeToken1",                 // BigDecimal!
+	"volumeUSD",                    // BigDecimal!
+	"untrackedVolumeUSD",           // BigDecimal!
+	"feesUSD",                      // BigDecimal!
+	"txCount",                      // BigInt!
+	"collectedFeesToken0",          // BigDecimal!
+	"collectedFeesToken1",          // BigDecimal!
+	"collectedFeesUSD",             // BigDecimal!
+	"totalValueLockedToken0",       // BigDecimal!
+	"totalValueLockedToken1",       // BigDecimal!
+	"totalValueLockedETH",          // BigDecimal!
+	"totalValueLockedUSD",          // BigDecimal!
+	"totalValueLockedUSDUntracked", // BigDecimal!
+	"liquidityProviderCount",       // BigInt!
+	// "poolHourData", // [PoolHourData!]! @derivedFrom(field: "pool")
+	// "poolDayData", // [PoolDayData!]! @derivedFrom(field: "pool")
+	// "mints", // [Mint!]! @derivedFrom(field: "pool")
+	// "burns", // [Burn!]! @derivedFrom(field: "pool")
+	// "swaps", // [Swap!]! @derivedFrom(field: "pool")
+	// "collects", // [Collect!]! @derivedFrom(field: "pool")
+	// "ticks", // [Tick!]! @derivedFrom(field: "pool")
+}
+
+type TokenResponse struct {
+	Token Token
 }
 
 type Token struct {
@@ -92,40 +132,25 @@ type Token struct {
 	TotalValueLockedUSD          string
 	TotalValueLockedUSDUntracked string
 	DerivedETH                   string
+	// other fields
 }
 
 var TokenFields []string = []string{
-	"id",
-	"symbol",
-	"name",
-	"decimals",
-	"totalSupply",
-	"volume",
-	"volumeUSD",
-	"untrackedVolumeUSD",
-	"feesUSD",
-	"txCount",
-	"poolCount",
-	"totalValueLocked",
-	"totalValueLockedUSD",
-	"totalValueLockedUSDUntracked",
-	"derivedETH",
-	// "whitelistPools",
-	// "tokenDayData",
+	"id",                           // ID!
+	"symbol",                       // String!
+	"name",                         // String!
+	"decimals",                     // BigInt!
+	"totalSupply",                  // BigInt!
+	"volume",                       // BigDecimal!
+	"volumeUSD",                    // BigDecimal!
+	"untrackedVolumeUSD",           // BigDecimal!
+	"feesUSD",                      // BigDecimal!
+	"txCount",                      // BigInt!
+	"poolCount",                    // BigInt!
+	"totalValueLocked",             // BigDecimal!
+	"totalValueLockedUSD",          // BigDecimal!
+	"totalValueLockedUSDUntracked", // BigDecimal!
+	"derivedETH",                   // BigDecimal!
+	// "whitelistPools", // [Pool!]!
+	// "tokenDayData", // [TokenDayData!]! @derivedFrom(field: "token")
 }
-
-// type Pool struct {
-// 	ID        any
-// 	Tick      int
-// 	FeeTier   int
-// 	SqrtPrice int
-// 	Liquidity int
-// 	Token0    Token
-// 	Token1    Token
-// }
-
-// type Token struct {
-// 	ID       any
-// 	Symbol   string
-// 	Decimals int
-// }
