2c1b03cd6333292e26a59d8bee0bdff14addfd13
Emerson Macro <emersonmacro@gmail.com>
2023-10-09 22:22:23 -0700

    feat: add unit tests for json converter functions

--- converter/converter_test.go
+++ converter/converter_test.go
@@ -1,8 +1,11 @@
 package converter
 
 import (
+	"math"
 	"math/big"
 	"testing"
+
+	unigraphclient "github.com/emersonmacro/go-uniswap-subgraph-client"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -42,5 +45,51 @@ func TestStringToBigFloat(t *testing.T) {
 		_, err := StringToBigFloat(s)
 		assert.NotNil(t, err)
 		assert.Contains(t, err.Error(), "unable to convert")
+	})
+}
+
+func TestModelToJsonBytes(t *testing.T) {
+	t.Run("when valid", func(t *testing.T) {
+		model := unigraphclient.Transaction{
+			ID:          "test",
+			BlockNumber: "1",
+			Timestamp:   "1",
+			GasUsed:     "1",
+			GasPrice:    "1",
+		}
+
+		expected := []byte("{\"id\":\"test\",\"blockNumber\":\"1\",\"timestamp\":\"1\",\"gasUsed\":\"1\",\"gasPrice\":\"1\"}")
+
+		bytes, err := ModelToJsonBytes(model)
+		assert.Nil(t, err)
+		assert.Equal(t, expected, bytes)
+	})
+
+	t.Run("when invalid", func(t *testing.T) {
+		_, err := ModelToJsonBytes(math.Inf(1))
+		assert.NotNil(t, err)
+	})
+}
+
+func TestModelToJsonString(t *testing.T) {
+	t.Run("when valid", func(t *testing.T) {
+		model := unigraphclient.Transaction{
+			ID:          "test",
+			BlockNumber: "1",
+			Timestamp:   "1",
+			GasUsed:     "1",
+			GasPrice:    "1",
+		}
+
+		expected := "{\"id\":\"test\",\"blockNumber\":\"1\",\"timestamp\":\"1\",\"gasUsed\":\"1\",\"gasPrice\":\"1\"}"
+
+		bytes, err := ModelToJsonString(model)
+		assert.Nil(t, err)
+		assert.Equal(t, expected, bytes)
+	})
+
+	t.Run("when invalid", func(t *testing.T) {
+		_, err := ModelToJsonString(math.Inf(1))
+		assert.NotNil(t, err)
 	})
 }
